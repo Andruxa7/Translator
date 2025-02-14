bf1b055ebe4359b09624284e004eaac916bc36cb
Andruxa7 <a.stecenko7@gmail.com>
2025-02-14 23:14:31 +0200

    Add TranslatorApp project

---
+++ Translator/Translator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Translator
+//
+//  Created by Andrii Stetsenko on 14.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Translator/Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Andrii Stetsenko on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
