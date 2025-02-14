bc0bf596140ff215c9a24577995f5bea6451e00c
Andruxa7 <a.stecenko7@gmail.com>
2025-02-14 23:40:21 +0200

    Added custom font in Info.plist and Constants and Extensions files

---
+++ Translator/Translator/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Translator
+//
+//  Created by Andrii Stetsenko on 14.02.2025.
+//
+
+import Foundation
+import SwiftUI
+
+let customFont = "KonkhmerSleokchher-Regular"
+let backgroundGradient = LinearGradient(colors: [Color(hex: 0xC9FFE0), Color(hex: 0xF3F5F6)], startPoint: .bottom, endPoint: .top)
---
+++ Translator/Translator/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  Translator
+//
+//  Created by Andrii Stetsenko on 14.02.2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex & 0xff0000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 8) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
