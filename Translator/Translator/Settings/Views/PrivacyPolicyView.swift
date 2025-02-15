eb362147a199a49866b4920593c0017eb2164f45
Andruxa7 <a.stecenko7@gmail.com>
2025-02-15 12:05:09 +0200

    Created RateUsView, ShareAppView, ContactUsView, RestorePurchasesView, PrivacyPolicyView, TermsOfUseView

---
+++ Translator/Translator/Settings/Views/PrivacyPolicyView.swift
@@ -0,0 +1,64 @@
+//
+//  PrivacyPolicyView.swift
+//  Translator
+//
+//  Created by Andrii Stetsenko on 15.02.2025.
+//
+
+import SwiftUI
+
+struct PrivacyPolicyView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                    .ignoresSafeArea(edges: .all)
+                
+                VStack {
+                    Text("Privacy Policy View")
+                }
+                .frame(width: geometry.size.width / 2,
+                       height: geometry.size.height / 5)
+                .background(Color.green.opacity(0.4))
+                .foregroundColor(Color.primary)
+                .cornerRadius(20)
+            }
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        withAnimation {
+                            dismiss()
+                        }
+                    } label: {
+                        ZStack {
+                            Image(systemName: "circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 48, height: 48)
+                                .foregroundStyle(.white)
+                            
+                            Image("close")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                        }
+                        .padding(.top, 30)
+                    }
+                }
+                
+                ToolbarItem(placement: .principal) {
+                    Text("Privacy Policy")
+                        .font(.custom(customFont, size: 32))
+                        .padding(.top, 70)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PrivacyPolicyView()
+}
---
+++ Translator/Translator/Settings/Views/RateUsView.swift
@@ -0,0 +1,64 @@
+//
+//  RateUsView.swift
+//  Translator
+//
+//  Created by Andrii Stetsenko on 15.02.2025.
+//
+
+import SwiftUI
+
+struct RateUsView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                    .ignoresSafeArea(edges: .all)
+                
+                VStack {
+                    Text("Rate Us View")
+                }
+                .frame(width: geometry.size.width / 2,
+                       height: geometry.size.height / 5)
+                .background(Color.green.opacity(0.4))
+                .foregroundColor(Color.primary)
+                .cornerRadius(20)
+            }
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        withAnimation {
+                            dismiss()
+                        }
+                    } label: {
+                        ZStack {
+                            Image(systemName: "circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 48, height: 48)
+                                .foregroundStyle(.white)
+                            
+                            Image("close")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                        }
+                        .padding(.top, 30)
+                    }
+                }
+                
+                ToolbarItem(placement: .principal) {
+                    Text("Rate Us")
+                        .font(.custom(customFont, size: 32))
+                        .padding(.top, 70)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    RateUsView()
+}
